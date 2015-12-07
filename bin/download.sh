d6d2a50a45f78e58931d68c79d3e33813d64455a
Adam Gagorik <adam.gagorik@gmail.com>
2015-12-07 12:30:23 -0500

    Fix download script to use subdirectory

--- bin/download.sh
+++ bin/download.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-cd ~
+#!/bin/bash -e
+TOPLEVEL=`git rev-parse --show-toplevel`
+
+cd "${TOPLEVEL}" 
+
 mkdir pocketmine
 cd ./pocketmine
+
 wget -q -O - https://raw.githubusercontent.com/PocketMine/php-build-scripts/master/installer.sh | bash -s - -v stable
 
 # stable
